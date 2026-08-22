26c5ca7691f6c75b2d058667635c3f57264db38a
Jesse Tane <jesse.tane@gmail.com>
2026-08-22 13:58:49 -0400

    move ws server to own module

---
+++ webServer.js
@@ -0,0 +1,125 @@
+#!/usr/bin/node
+
+import tcp from 'net'
+import ggencoder from 'ggencoder'
+import { WebSocketServer } from 'ws'
+
+const env = process.env
+const aisHost = env.AIS_HOST || '::1'
+const aisPort = env.AIS_PORT || 9000
+const aisReconnectInterval = parseInt(env.AIS_RECONNECT || 5000)
+let wss = null
+let wsConnections = []
+const wsHost = env.WS_HOST || '::'
+const wsPort = env.WS_PORT || 9001
+const renderRate = 1000
+let renderTimeout = null
+const renderShipStatusRate = 2500
+const stale = 1000 * 60 * 2
+const dead = 1000 * 60 * 5
+const ships = new Map()
+let buffer = ''
+
+openAisSocket()
+openWsServer()
+setInterval(renderShipStatus, renderShipStatusRate)
+
+function openAisSocket () {
+	const socket = tcp.connect(aisPort, aisHost)
+	socket.on('data', d => {
+		// console.log(`got ${d.length} bytes`, d.toString())
+		buffer += d.toString()
+		requestRender()
+	})
+	socket.on('close', () => {
+		setTimeout(openAisSocket, aisReconnectInterval)		
+	})
+}
+
+function openWsServer () {
+	wss = new WebSocketServer({ port: wsPort, host: wsHost })
+	wss.on('connection', ws => {
+		console.log('got ws connection')
+		wsConnections.push(ws)
+		console.log('total ws connections:', wsConnections.length)
+		ws.on('close', () => {
+			close()
+		})
+		ws.on('error', err => {
+			console.log('ws connection error:', err)
+			close()
+		})
+		function close () {
+			console.log('ws connection close')
+			wsConnections = wsConnections.filter(c => c !== ws)
+			console.log('total ws connections:', wsConnections.length)
+		}
+	})
+	wss.on('listening', () => {
+		console.log('websocket server listening at:', wss.address())
+	})
+}
+
+function requestRender () {
+	if (renderTimeout) return
+	renderTimeout = setTimeout(render, renderRate)
+}
+
+function render () {
+	const lines = buffer.split('\r\n')
+	buffer = lines.at(-1)
+	if (lines[0][0] !== '!') {
+		lines.shift()
+	}
+	const changes = []
+	for (let m of lines) {
+		const ship = updateShip(m)
+		if (ship) {
+			changes.push(ship)
+		}
+	}
+	if (changes.length) {
+		wssConnections.forEach(c => c.send(JSON.stringify(changes)))
+	}
+	renderTimeout = null
+	console.log(ships)
+}
+
+function renderShipStatus () {
+	const now = new Date()
+	for (const [mmsi, ship] of ships.entries()) {
+		const elapsed = now - ship.updated
+		if (elapsed >= dead) {
+			ships.delete(mmsi)
+			console.log(`ship dead: ${mmsi}`)
+		} else if (elapsed >= stale && ship.status !== 'inactive') {
+			ship.status = 'inactive'
+			console.log(`ship inactive: ${mmsi}`)
+		}
+	}
+}
+
+function updateShip (m) {
+	let ship;
+	try {
+		ship = new ggencoder.AisDecode(m)
+	} catch (err) {
+		console.error('updateShip: bad message', err)
+	}
+	if (!ship || !ship.valid || !ship.mmsi) {
+		console.error('updateShip: invalid ship:', ship)
+		return
+	}
+	delete ship.bitarray
+	delete ship.payload
+	const now = Date.now()
+	const mmsi = ship.mmsi
+	const currentShip = ships.get(mmsi) || { mmsi, created: now }
+	ship = {
+		...currentShip,
+		...ship,
+		updated: now,
+		status: 'active',
+	}
+	ships.set(mmsi, ship)
+}
