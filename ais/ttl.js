490e2825be83c50ab39977015dfd70c74a2bc70d
Jesse Tane <jesse.tane@gmail.com>
2026-08-24 22:29:16 -0400

    ais/ttl: aton, base station ttl -> 24h

--- ais/ttl.js
+++ ais/ttl.js
@@ -6,15 +6,17 @@ export default {
 		maxAge: 1000 * 60 * 20,
 	},
 	2: { // Base Station (10s nominal)
-		oldAge: 1000 * 10 * factor,
-		maxAge: 1000 * 60 * 20,
+		// oldAge: 1000 * 10 * factor,
+		oldAge: 1000 * 60 * 60 * 1,
+		maxAge: 1000 * 60 * 60 * 24,
 	},
 	3: { // SAR Aircraft (10s nominal)
 		oldAge: 1000 * 10 * factor,
 		maxAge: 1000 * 60 * 5,
 	},
 	4: { // Aid-to-Navigation (3m nominal)
-		oldAge: 1000 * 60 * 3 * factor,
+		// oldAge: 1000 * 60 * 3 * factor,
+		oldAge: 1000 * 60 * 60 * 1,
 		maxAge: 1000 * 60 * 60 * 24,
 	},
 	5: { // AIS-SART (1m nominal)
